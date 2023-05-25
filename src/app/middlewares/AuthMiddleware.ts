0025570f8c95edce7537404bf644c653904264a6
kaloca <mega.kaloca@gmail.com>
2023-05-25 16:16:50 -0700

    assures user owns token

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
 import { AUTH_SECRET } from '../util/secrets'
+interface MyJwtPayload extends JwtPayload {
+	userId: string
+}
 
 export const requireAuth = (
 	req: Request,
@@ -21,10 +24,18 @@ export const requireAuth = (
 	try {
 		const base64SecretKey = AUTH_SECRET as string
 		const secretKey = Buffer.from(base64SecretKey, 'base64').toString('utf8')
-		const decodedToken = jwt.verify(token, secretKey)
+		const decodedToken = jwt.verify(token, secretKey) as MyJwtPayload
 		;(req as any).user = decodedToken
+
+		if (req.user?.userId && req.user?.userId !== decodedToken.userId) {
+			return res.status(403).json({
+				message: 'User ID from token does not match user ID from request.',
+			})
+		}
+
 		next()
 	} catch (error) {
+		console.log(error)
 		return res.status(401).json({ message: 'Invalid or expired token.' })
 	}
 }
