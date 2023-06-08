1a95427f8bb587637d846511475fd554f6747d18
kaloca <mega.kaloca@gmail.com>
2023-06-08 00:19:14 -0700

    preparing for aws deployment

--- dist/app/models/JobTypeModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const JobTypeSchema = new mongoose_1.Schema({
-    type: { type: String, required: true, unique: true },
-});
-exports.default = (0, mongoose_1.model)('JobType', JobTypeSchema);
-//# sourceMappingURL=JobTypeModel.js.map
