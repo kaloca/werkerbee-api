11bcfb9e5a455b107eec76e765c3e1ded5b5ec4a
kaloca <mega.kaloca@gmail.com>
2023-06-07 23:57:13 -0700

    added clock and break functionality

--- src/app/controllers/Jobs/JobController.ts
+++ src/app/controllers/Jobs/JobController.ts
@@ -19,6 +19,122 @@ const getJob = async (req: Request, res: Response) => {
 	}
 }
 
-const JobController = { getJob }
+const clockIn = async (req: Request, res: Response) => {
+	const jobId = req.params.jobId
+	const workerId = req.user?.userId
+
+	try {
+		const job: IJob | null = await JobModel.findById(jobId)
+
+		if (!job) {
+			return res.status(404).json({ message: 'Job not found' })
+		}
+
+		if (job.workerId != workerId) {
+			return res.status(401).json({ message: 'Unauthorized' })
+		}
+		const now = new Date()
+		const shiftStart = new Date(job.shiftStart)
+		const diff = (now.getTime() - shiftStart.getTime()) / 1000 / 60
+
+		if (diff < 30) {
+			return res.status(400).json({
+				message: 'You can only clock in within 30 minutes of shift start time',
+				diff,
+			})
+		}
+
+		job.clockStart = now
+
+		await job.save()
+
+		res.status(200).json(diff)
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: 'An error occurred while fetching job types.', error })
+	}
+}
+
+const clockOut = async (req: Request, res: Response) => {
+	const jobId = req.params.jobId
+	const workerId = req.user?.userId
+
+	try {
+		const job: IJob | null = await JobModel.findById(jobId)
+
+		if (!job) {
+			return res.status(404).json({ message: 'Job not found' })
+		}
+
+		if (job.workerId != workerId) {
+			return res.status(401).json({ message: 'Unauthorized' })
+		}
+
+		job.clockEnd = new Date()
+		await job.save()
+
+		res.status(200).json(job)
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: 'An error occurred while fetching job types.', error })
+	}
+}
+
+const startBreak = async (req: Request, res: Response) => {
+	const jobId = req.params.jobId
+	const workerId = req.user?.userId
+
+	try {
+		const job: IJob | null = await JobModel.findById(jobId)
+
+		if (!job) {
+			return res.status(404).json({ message: 'Job not found' })
+		}
+
+		if (job.workerId != workerId) {
+			return res.status(401).json({ message: 'Unauthorized' })
+		}
+
+		const newBreak = { start: new Date() }
+		job.breaks ? job.breaks.push(newBreak) : (job.breaks = [newBreak])
+		await job.save()
+
+		res.status(200).json(job)
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: 'An error occurred while fetching job types.', error })
+	}
+}
+
+const endBreak = async (req: Request, res: Response) => {
+	const jobId = req.params.jobId
+	const workerId = req.user?.userId
+	const breakId = req.params.breakId
+
+	try {
+		const job = await JobModel.findById(jobId)
+		if (!job) return res.status(404).json({ message: 'Job not found' })
+		if (job.workerId != workerId)
+			return res.status(401).json({ message: 'Unauthorized' })
+		if (!job.breaks) return res.status(404).json({ message: 'Break not found' })
+		const breakIndex = job.breaks.findIndex(
+			(breakItem) => breakItem._id?.toString() === breakId
+		)
+		if (breakIndex === -1) return res.status(404).send('Break not found')
+
+		job.breaks[breakIndex].end = new Date()
+
+		await job.save()
+
+		res.status(200).send(job)
+	} catch (error) {
+		res.status(500).send(error)
+	}
+}
+
+const JobController = { getJob, clockIn, clockOut, startBreak, endBreak }
 
 export default JobController
--- src/app/interfaces/models/Job.ts
+++ src/app/interfaces/models/Job.ts
@@ -3,12 +3,21 @@ import { Document } from 'mongoose'
 export interface IJob extends Document {
 	name: string
 	rating?: number
-	worker: string
-	company: string
-	jobPosting: string
+	workerId: string
+	companyId: string
+	jobPostingId: string
 	status: 'PENDING' | 'COMPLETE' | 'CANCELED'
 	shiftStart: Date
 	shiftEnd: Date
+	clockStart?: Date
+	clockEnd?: Date
+	breaks?: [
+		{
+			start: Date
+			end?: Date
+			_id?: string
+		}
+	]
 	createdAt: Date
 	updatedAt: Date
 }
