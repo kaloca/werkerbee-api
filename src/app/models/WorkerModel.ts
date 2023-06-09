008ac42ceaf6e86a1b8b14b144e28ad07f885491
Gabriel Noya <mega.kaloca@gmail.com>
2023-06-09 02:57:53 -0700

    added timestamps to worker model

--- src/app/models/WorkerModel.ts
+++ src/app/models/WorkerModel.ts
@@ -12,28 +12,31 @@ const experienceSchema: Schema = new Schema({
 	endDate: { type: Date, required: true },
 })
 
-const workerSchema: Schema = new Schema({
-	name: { type: String, required: true },
-	username: {
-		type: String,
-		required: true,
-		unique: true,
+const workerSchema: Schema = new Schema(
+	{
+		name: { type: String, required: true },
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		bio: { type: String, required: false },
+		phoneNumber: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		location: { type: pointSchema, required: true },
+		address: { type: addressSchema, required: true, select: false },
+		billingAddress: { type: addressSchema, required: false, select: false },
+		bankInfo: { type: bankAccountSchema, required: false, select: false },
+		ssn: { type: String, required: true, unique: true, select: false },
+		birthday: { type: Date, required: true },
+		rating: { type: Number, default: null },
+		jobTypes: { type: [String], required: true },
+		experiences: { type: [experienceSchema], required: false },
+		hashedPassword: { type: String, required: true, select: false },
+		// documents: { type: Buffer, required: true },
 	},
-	bio: { type: String, required: false },
-	phoneNumber: { type: String, required: true, unique: true },
-	email: { type: String, required: true, unique: true },
-	location: { type: pointSchema, required: true },
-	address: { type: addressSchema, required: true, select: false },
-	billingAddress: { type: addressSchema, required: false, select: false },
-	bankInfo: { type: bankAccountSchema, required: false, select: false },
-	ssn: { type: String, required: true, unique: true, select: false },
-	birthday: { type: Date, required: true },
-	rating: { type: Number, default: null },
-	jobTypes: { type: [String], required: true },
-	experiences: { type: [experienceSchema], required: false },
-	hashedPassword: { type: String, required: true, select: false },
-	// documents: { type: Buffer, required: true },
-})
+	{ timestamps: true }
+)
 
 workerSchema.index({ location: '2dsphere' })
 
